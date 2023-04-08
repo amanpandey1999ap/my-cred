411835174ffeb352be87f886e68f88e1223e21eb
amanpandey <aman.pandey@mutualmobile.com>
2023-04-09 00:01:34 +0530

    Main Activity rename

--- ui/src/main/java/com/aman/mycred/ui/HomeActivity.kt
+++ ui/src/main/java/com/aman/mycred/ui/HomeActivity.kt
@@ -6,14 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.aman.mycred.ui.screens.landing.LandingScreen
 import com.aman.mycred.ui.theme.MyCredTheme
 
-class MainActivity : ComponentActivity() {
+class HomeActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
